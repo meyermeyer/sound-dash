9d4174f4ab62833bc534cbe542327e6cfe058b5f
Meyer <ehlmeyer@gmail.com>
2019-05-28 14:40:14 -0500

    login works in postman

--- server/strategies/user.strategy.js
+++ server/strategies/user.strategy.js
@@ -8,7 +8,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  pool.query('SELECT * FROM "user" WHERE id = $1', [id]).then((result) => {
+  pool.query('SELECT * FROM "users" WHERE id = $1', [id]).then((result) => {
     // Handle Errors
     const user = result && result.rows && result.rows[0];
 
@@ -33,7 +33,7 @@ passport.deserializeUser((id, done) => {
 
 // Does actual work of logging in
 passport.use('local', new LocalStrategy((username, password, done) => {
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username])
+    pool.query('SELECT * FROM "users" WHERE username = $1', [username])
       .then((result) => {
         const user = result && result.rows && result.rows[0];
         if (user && encryptLib.comparePassword(password, user.password)) {
@@ -47,7 +47,7 @@ passport.use('local', new LocalStrategy((username, password, done) => {
           done(null, null);
         }
       }).catch((error) => {
-        console.log('Error with query for user ', error);
+        console.log('Error with query for users ', error);
         // done takes an error (we have one) and a user (null in this case)
         // this will result in the server returning a 500 status code
         done(error, null);
