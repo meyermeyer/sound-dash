77ffcccb3f9d11a90845ed73e0013aae886e1b2f
Meyer <ehlmeyer@gmail.com>
2019-05-28 14:38:37 -0500

    register works in postman

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,13 +16,18 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {  
+  console.log('in POST /api/user/register', req.body)
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO "user" (username, password) VALUES ($1, $2) RETURNING id';
+  const queryText = 'INSERT INTO "users" (username, password) VALUES ($1, $2) RETURNING id';
   pool.query(queryText, [username, password])
     .then(() => res.sendStatus(201))
-    .catch(() => res.sendStatus(500));
+    .catch((error) => {
+      console.log('error in POST /api/user/register', error);
+      res.sendStatus(500);
+      
+    })
 });
 
 // Handles login form authenticate/login POST
