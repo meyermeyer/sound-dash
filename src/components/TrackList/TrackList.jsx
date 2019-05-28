cb7c902764c213fd37f9870580ebc09e680b60b4
Meyer <ehlmeyer@gmail.com>
2019-05-28 13:03:29 -0500

    created TrackItem component

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import TrackItem from '../TrackItem/TrackItem'
+
 class TrackList extends Component {
     render() {
         return(
             <ul>
-                <li>tracks here</li>
+                <TrackItem />
             </ul>
         )
     }
